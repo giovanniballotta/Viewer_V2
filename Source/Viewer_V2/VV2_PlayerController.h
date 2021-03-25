0dea67801976ca94f10bbdc167f4928c0cafceaf
giovanniballotta <giovanni@giovanniballotta.com>
2021-03-25 23:23:23 +0000

    Commit Iniziale Codice

---
+++ Source/Viewer_V2/VV2_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "VV2_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIEWER_V2_API AVV2_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
